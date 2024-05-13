f4cc71728f7be875197a2e28cf5ac41de638d214
Oscar Roman <oscar.bastidas@bayport.com.co>
2024-05-13 17:10:42 -0500

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import {
     Tabs
 } from '@/components/ui'
 import FormCreateRequest from "@/components/formCreateRequest"
+import FormLimitDepot from "@/components/formLimitDepot"
 
 const getProviders = async () => {
     const res = await fetch('https://64b68442df0839c97e15b2a0.mockapi.io/api/v1/provider');
@@ -39,16 +40,9 @@ export default async function Home() {
         {
             title: 'Límite de depósito',
             content: (
-                <Card>
-                    <h1 className='text-3xl text-blue-600'>Title Test 2</h1>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-                        aperiam asperiores doloribus velit dolore magnam ex consectetur fugit
-                        earum illum qui similique architecto dolorum, minima enim quidem
-                        voluptatibus at nulla deleniti harum! Totam, mollitia quos voluptatem
-                        deleniti provident obcaecati rerum.
-                    </p>
-                </Card>
+                <div>
+                    <FormLimitDepot />
+                </div>
             ),
         },
     ]
---
+++ src/components/formLimitDepot.tsx
@@ -0,0 +1,11 @@
+import {Card} from "@/components/ui"
+
+export default function FormLimitDepot() {
+    return (
+        <div>
+            <Card>
+                <h2>DEFINA SUS LÍMITES DE DEPÓSITO</h2>
+            </Card>
+        </div>
+    )
+}
